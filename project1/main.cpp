3251daf8cf390d084e66979a90267fa8e867fd9e
fmendezr <mendez.felixrn@gmail.com>
2024-01-26 13:38:42 -0500

    Initial Commit

---
+++ project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by Felix Mendez on 1/26/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
